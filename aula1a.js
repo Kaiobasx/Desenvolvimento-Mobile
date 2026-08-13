fb8d1c15bd2f8c3591fb566e5116b0e13f30d4c0
Kaio Oliveira <kaiooliveira528@gmail.com>
2026-08-13 01:00:27 +0000

    first commit

---
+++ aula1a.js
@@ -0,0 +1,6 @@
+let nome = "João";
+let idade = 30;
+
+let mensagem = `Olá, meu nome é ${nome} e eu tenho ${idade} anos.`;
+
+console.log(mensagem);
---
+++ aula1d.js
@@ -0,0 +1,5 @@
+let multiplicar = function(a, b) {
+    return a * b;
+};
+
+console.log("Resultado da multiplicação: " + multiplicar(3, 4));
